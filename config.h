64ea1802ddc071dacc4846877117cfd7c9570095
Richard <git.nordstrom@gmail.com>
2023-03-18 14:43:19 +0100

    patch partial dwm-centeredmaster-6.1.diff

--- config.h
+++ config.h
@@ -46,6 +46,8 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 	{ "[D]",      deck },
     { "|||",      col },
+	{ "|M|",      centeredmaster },
+
 };
 
 /* key definitions */
@@ -86,6 +88,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
     { MODKEY,                       XK_y,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[5]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
