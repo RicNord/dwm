7375ac167109ca983743c5459010a171c8e8864e
Richard <git.nordstrom@gmail.com>
2023-03-19 01:03:45 +0100

    Revert "patch partial dwm-centeredmaster-6.1.diff"
    This reverts commit 64ea1802ddc071dacc4846877117cfd7c9570095.

--- config.h
+++ config.h
@@ -46,8 +46,6 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 	{ "[D]",      deck },
     { "|||",      col },
-	{ "|M|",      centeredmaster },
-
 };
 
 /* key definitions */
@@ -91,7 +89,6 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
     { MODKEY,                       XK_y,      setlayout,      {.v = &layouts[4]} },
-	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[5]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
